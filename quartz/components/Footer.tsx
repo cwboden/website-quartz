adada5ca434406525ba281cd26b0f0f1bb3f903e
Carson Boden <carson.boden@gmail.com>
2024-03-08 01:26:41 -0800

    feat: Use icons for links.

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,17 +14,19 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <hr />
-        <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
-        </p>
         <ul>
-          {Object.entries(links).map(([text, link]) => (
+          {Object.entries(links).map(([text, detail]) => {
+            const fontclass = detail.icon
+            const iconstyle = "color: " + detail.iconcolor
+            return (
             <li>
-              <a href={link}>{text}</a>
+              <a href={detail.link}><i style={iconstyle} class={fontclass}/></a>
             </li>
-          ))}
+          )})}
         </ul>
+        <p>
+          <i class="fa-regular fa-copyright"/>{year} | Created with <a href="https://obsidian.md">Obsidian</a>, <a href="https://quartz.jzhao.xyz/">Quartz</a>, and Love 💖
+        </p>
       </footer>
     )
   }
